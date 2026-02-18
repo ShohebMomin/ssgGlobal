0835ed027f4c1bf42ce37f20acad89673a530ac9
Shoaib <mominshoaib4@gmail.com>
2026-02-18 15:52:16 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+
 import Navbar from './components/Navbar'
 import HomePage from './components/Pages/HomePage'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import AboutUs from './components/Pages/AboutUs';
 import AboutPuja from './components/Pages/AboutPuja';
 import BlogsPage from './components/Pages/BlogsPage';
@@ -16,8 +16,6 @@ import Blog from './components/Pages/Blog';
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       {/* Optional: show on all pages */}
@@ -30,15 +28,15 @@ function App() {
         <Route path="/about-puja" element={<AboutPuja />} />
         <Route path="/blogs" element={<BlogsPage />} />
         <Route path="/contact-us" element={<ContactUs />} />
-        <Route path="/about-swamiji" element={<AboutSwamiji/>} />
-        <Route path="/social-cause" element={<SocialCause/>} />
+        <Route path="/about-swamiji" element={<AboutSwamiji />} />
+        <Route path="/social-cause" element={<SocialCause />} />
         <Route path="/blog/:id" element={<Blog />} />
         {/* <Route path="*" element={<HomePage />} /> */}
       </Routes>
 
       <Footer />
     </>
-    
+
   )
 }
 
