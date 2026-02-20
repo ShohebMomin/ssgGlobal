099785cb5d802563166262d6b1f06dc5c9744d2d
Shoaib <mominshoaib4@gmail.com>
2026-02-20 13:55:25 +0530

    All images moved to public folder

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-const HeroSection = (props) => {
-  const className = `absolute  ${props.layout} flex items-center  text-white ${props.textSize} font-playfair font-bold md:leading-15`
-  const Imagesrc = `${props.imagesrc}`
+
+
+const HeroSection = ({ imagesrc, layout, textSize, heading }) => {
+  const className = `absolute  ${layout} flex items-center  text-white ${textSize} font-playfair font-bold md:leading-15`
+  // const Imagesrc = `${props.imagesrc}`
 
   return (
     <>
 
       <div className="overflow-hidden w-full h-full relative">
-        <img className="w-full h-full" src={Imagesrc} alt="unknown" />
+        <img className="w-full h-full" src={imagesrc} alt="unknown" />
         <div className={className}>
-          {props.heading}
+          {heading}
         </div>
       </div>
 
--- client/src/components/Pages/AboutPuja.jsx
+++ client/src/components/Pages/AboutPuja.jsx
@@ -6,7 +6,7 @@ import RandomImageContainer from '../RandomImageContainer'
 const AboutPuja = () => {
   return (
     <>
-      <HeroSection textSize="text-xs md:text-5xl" imagesrc="src\assets\Images\aboutpuja.jpg" layout="top-2 md:top-10 left-auto right-5 md:right-10 h-full w-20 md:w-96" heading="Discover the sacred ritual that blesses every aspect of your life with the divine grace of the eight forms of Goddess Lakshmi."/>
+      <HeroSection textSize="text-xs md:text-5xl" imagesrc="public\aboutpuja.jpg" layout="top-2 md:top-10 left-auto right-5 md:right-10 h-full w-20 md:w-96" heading="Discover the sacred ritual that blesses every aspect of your life with the divine grace of the eight forms of Goddess Lakshmi." />
 
       {/* Mission Statement */}
 
@@ -14,87 +14,87 @@ const AboutPuja = () => {
         <HeadingParagraph text="text-customPurple text-base" heading="An Ancient Ritual for Modern Times" paragraph="The Ashtalakshmi Puja is a powerful and auspicious ritual rooted in Vedic traditions. It invokes the blessings of the eight manifestations of Goddess Lakshmi, each representing different facets of prosperity, including wealth, health, knowledge, courage, and spiritual growth.
 This puja is ideal for those seeking divine guidance to overcome challenges, achieve success, and bring harmony to their personal and professional lives."/>
       </section>
-        {/* image gallery  */}
-        <section>
-        <RandomImageContainer/>
-        </section>
+      {/* image gallery  */}
+      <section>
+        <RandomImageContainer />
+      </section>
 
-        {/* The Blessings of the Ashtalakshmi */}
-        <section>
-       <div  className="text-center"><HeadingParagraph text="text-customPurple" heading="The Blessings of the Ashtalakshmi"/></div>
-       <main className="mb-10">
+      {/* The Blessings of the Ashtalakshmi */}
+      <section>
+        <div className="text-center"><HeadingParagraph text="text-customPurple" heading="The Blessings of the Ashtalakshmi" /></div>
+        <main className="mb-10">
           <div className="md:flex  justify-center ashtalakshmi-div">
-              
-              <div className="ashtalakshmi-content-div"><img src="/src/assets/Images/adi-lakshmi.png" alt="" />
+
+            <div className="ashtalakshmi-content-div"><img src="public/adi-lakshmi.png" alt="" />
               <h2>Adi Lakshmi:</h2>
               <p>Bestows eternal wealth and spiritual strength.</p>
-              </div>
+            </div>
 
-              <div className="ashtalakshmi-content-div"><img src="/src/assets/Images/dhan-lakshmi.png" alt="" />
+            <div className="ashtalakshmi-content-div"><img src="public/dhan-lakshmi.png" alt="" />
               <h2>Dhana Lakshmi:</h2>
               <p>Brings financial abundance and prosperity.</p>
-              </div>
+            </div>
 
-              <div className="ashtalakshmi-content-div"><img src="/src/assets/Images/dhanya-lakshmi.png" alt="" />
+            <div className="ashtalakshmi-content-div"><img src="/src/assets/Images/dhanya-lakshmi.png" alt="" />
               <h2>Dhanya Lakshmi:</h2>
               <p>Blesses agricultural bounty and nourishment.</p>
-              </div>
+            </div>
 
-              <div className="ashtalakshmi-content-div"><img src="/src/assets/Images/gaja-lakshmi.png" alt="" />
+            <div className="ashtalakshmi-content-div"><img src="/src/assets/Images/gaja-lakshmi.png" alt="" />
               <h2>Gaja Lakshmi:</h2>
               <p>Grants royal power, success, and prestige.</p>
-              </div>
+            </div>
 
           </div>
 
           <div className="md:flex justify-center ashtalakshmi-div">
-            
-              <div className="ashtalakshmi-content-div"><img src="/src/assets/Images/santana-lakshmi.png" alt="" />
+
+            <div className="ashtalakshmi-content-div"><img src="/src/assets/Images/santana-lakshmi.png" alt="" />
               <h2>Santana Lakshmi:</h2>
               <p>Blesses with offspring and family harmony.</p>
-              </div>
+            </div>
 
-              <div className="ashtalakshmi-content-div"><img className="!w-16" src="/src/assets/Images/veera-lakshmi.png" alt="" />
+            <div className="ashtalakshmi-content-div"><img className="!w-16" src="/src/assets/Images/veera-lakshmi.png" alt="" />
               <h2>Veera Lakshmi:</h2>
               <p>Offers courage, strength, and resilience.</p>
-              </div>
+            </div>
 
-              <div className="ashtalakshmi-content-div"><img src="/src/assets/Images/vidya-lakshmi.png" alt="" />
+            <div className="ashtalakshmi-content-div"><img src="/src/assets/Images/vidya-lakshmi.png" alt="" />
               <h2>Vidya Lakshmi:</h2>
               <p>Embodies knowledge, wisdom, and education.</p>
-              </div>
+            </div>
 
-              <div className="ashtalakshmi-content-div"><img src="/src/assets/Images/vijiya-lakshmi.png" alt="" />
+            <div className="ashtalakshmi-content-div"><img src="/src/assets/Images/vijiya-lakshmi.png" alt="" />
               <h2>Vijaya Lakshmi:</h2>
               <p>Ensures victory and success in endeavors.</p>
-              </div>
+            </div>
 
           </div>
         </main>
-        </section>
+      </section>
 
-        {/* Why Perform the Ashtalakshmi Puja? */}
+      {/* Why Perform the Ashtalakshmi Puja? */}
 
-        <section className="text-center">
-          <HeadingParagraph heading="Why Perform the Ashtalakshmi Puja?" text="text-customPurple"/>
-            <div className="Why-perform-the-ashtalakshmi-puja md:p-10 px-5 md:justify-center flex text-left">
-                <ul className="space-y-4 list-disc list-inside pl-2 text-black md:text-xl">
-                    <li>Removes financial obstacles and promotes prosperity.</li>
-                    <li>Enhances spiritual growth and inner peace.</li>
-                    <li>
-                       Invokes blessings for family harmony and health.
-                    </li>
-                    <li>
-                       Ensures success in personal and professional endeavors.
-                    </li>
-                    <li>
-                       Brings peace, positivity, and divine energy into your life.
-                    </li>
-                </ul>
-            </div>
-        </section>
-        {/* CTA */}
-        <CTA heading="Ready to Experience the Divine Blessings?" buttonText="Reserve Your Puja"/>
+      <section className="text-center">
+        <HeadingParagraph heading="Why Perform the Ashtalakshmi Puja?" text="text-customPurple" />
+        <div className="Why-perform-the-ashtalakshmi-puja md:p-10 px-5 md:justify-center flex text-left">
+          <ul className="space-y-4 list-disc list-inside pl-2 text-black md:text-xl">
+            <li>Removes financial obstacles and promotes prosperity.</li>
+            <li>Enhances spiritual growth and inner peace.</li>
+            <li>
+              Invokes blessings for family harmony and health.
+            </li>
+            <li>
+              Ensures success in personal and professional endeavors.
+            </li>
+            <li>
+              Brings peace, positivity, and divine energy into your life.
+            </li>
+          </ul>
+        </div>
+      </section>
+      {/* CTA */}
+      <CTA heading="Ready to Experience the Divine Blessings?" buttonText="Reserve Your Puja" />
 
     </>
   )
