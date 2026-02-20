43b63f7178a1287872e1670816c6cd042da75c5a
Shoaib <mominshoaib4@gmail.com>
2026-02-20 13:25:00 +0530

    images src updated

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 const HeroSection = (props) => {
-    const className = `absolute  ${props.layout} flex items-center  text-white ${props.textSize} font-playfair font-bold md:leading-15`
-    const Imagesrc = `${props.imagesrc}`
+  const className = `absolute  ${props.layout} flex items-center  text-white ${props.textSize} font-playfair font-bold md:leading-15`
+  const Imagesrc = `${props.imagesrc}`
 
   return (
-    <div className="overflow-hidden w-full h-full relative">
-              <img className="w-full h-full" src={Imagesrc} alt="unknown" />
-              <div  className={className}>
-                {props.heading}
-              </div>
-    </div>
+    <>
+
+      <div className="overflow-hidden w-full h-full relative">
+        <img className="w-full h-full" src={Imagesrc} alt="unknown" />
+        <div className={className}>
+          {props.heading}
+        </div>
+      </div>
+
+    </>
   )
-  HeroSection.defaultProps = {
-   layout: "absolute top-28 md:left-15 left-auto h-auto w-20 md:w-96",
-   textSize: "text-xs md:text-4xl"
-  };
 }
 
 export default HeroSection
--- client/src/components/Pages/BlogsPage.jsx
+++ client/src/components/Pages/BlogsPage.jsx
@@ -1,45 +1,43 @@
 import HeroSection from "../HeroSection"
-import React, {useState, useEffect} from 'react'
-import {useNavigate} from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from "react-router-dom";
 
 const BlogsPage = () => {
   const navigate = useNavigate();
-  const goToBlog = ({blogs}) =>{
-    
-  }
+
 
   const [blogs, setBlogs] = useState([])
 
   useEffect(() => {
     fetch("/blogdata.json")
-    .then((res) => res.json())
-    .then((data) => setBlogs(data))
-    .catch((err) => console.log("error loading data: ",err))
+      .then((res) => res.json())
+      .then((data) => setBlogs(data))
+      .catch((err) => console.log("error loading data: ", err))
   }, [])
-  
+
 
   return (
     <>
-      <HeroSection textSize="text-xs md:text-2xl " imagesrc="src\assets\Images\book.jpg" layout="top-8 md:top-52 left-2 md:left-20" heading="Discover the sacred ritual that blesses every aspect of your life with the divine grace of the eight forms of Goddess Lakshmi."/>
+      <HeroSection textSize="text-xs md:text-2xl " imagesrc="src\assets\Images\book.jpg" layout="top-8 md:top-52 left-2 md:left-20" heading="Discover the sacred ritual that blesses every aspect of your life with the divine grace of the eight forms of Goddess Lakshmi." />
 
       <div className="md:px-10 px-1 mx-auto my-5 grid grid-cols-1 md:grid-cols-3 gap-6">
-      
+
         {blogs.map((blogs, index) => (
           <div key={index} className="bg-[#fffff0] p-4 rounded-2xl  place-items-end-safe overflow-visible md:w-md lg:w-md ">
-              <img key={index}
-                src="src/assets/Images/spiritual.jpg"
-                alt={blogs.name}
-                className="md:w-96 lg:w-96 object-cover rounded-2xl m-auto"
-              />
-              <div className="p-4">
-                <p className=" text-black">{blogs.date}</p>
-                <h3 className="mt-2 text-xl font-semibold">{blogs.heading}</h3>
-                <p className="mt-2 text-sm">{blogs.information}</p>
-              </div>
-              <div className="flex font-playfair text-xl font-bold"><button onClick={() => navigate(`/blog/${blogs.id}`)} className="cursor-pointer mr-2 text-[#D4AF37]">Read More</button><p className="text-customPurple font-lato font-bold ">&gt;&gt;</p></div>
+            <img key={index}
+              src="src/assets/Images/spiritual.jpg"
+              alt={blogs.name}
+              className="md:w-96 lg:w-96 object-cover rounded-2xl m-auto"
+            />
+            <div className="p-4">
+              <p className=" text-black">{blogs.date}</p>
+              <h3 className="mt-2 text-xl font-semibold">{blogs.heading}</h3>
+              <p className="mt-2 text-sm">{blogs.information}</p>
             </div>
+            <div className="flex font-playfair text-xl font-bold"><button onClick={() => navigate(`/blog/${blogs.id}`)} className="cursor-pointer mr-2 text-[#D4AF37]">Read More</button><p className="text-customPurple font-lato font-bold ">&gt;&gt;</p></div>
+          </div>
         ))}
-    </div>
+      </div>
     </>
   )
 }
